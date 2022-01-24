52cfafad1a3cd246f59271f8e6a0102c829a6007
Mtchuikov <71787586+Mtchuikov@users.noreply.github.com>
2022-01-24 13:12:06 +0300

    Update deleteuser.go

--- internal/db/mongodb/deleteuser.go
+++ internal/db/mongodb/deleteuser.go
@@ -5,22 +5,16 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"web3pay/internal/db"
 )
-
 func (c *Client) DeleteUser(ctx context.Context, userId string) (*db.User, error) {
 	coll := c.m.Database("payment").Collection("userInfo")
 	result := coll.FindOneAndDelete(
 		ctx,
-		bson.D{
-			{"userId", userId},
-		},
+		bson.M{"userId": userId},
 	)
 	if result.Err() != nil {
 		return nil, result.Err()
 	}
 	var deletedUser *db.User
 	err := result.Decode(&deletedUser)
-	if err != nil {
-		return nil, err
-	}
-	return deletedUser, nil
+	return deletedUser, err
 }
