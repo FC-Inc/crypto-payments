94e40d3a9d7d6c11adfb0ac8c04552469cde08e6
Mtchuikov <71787586+Mtchuikov@users.noreply.github.com>
2022-01-24 13:11:20 +0300

    Update client.go

--- internal/db/mongodb/client.go
+++ internal/db/mongodb/client.go
@@ -28,6 +28,7 @@ func (c *Client) Connect() error {
 }
 
 func (c *Client) Disconnect() {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	c.m.Disconnect(ctx)
 }
