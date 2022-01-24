bff54b4d32a1cdf7b4d77c418bd05ab14f64d425
Mtchuikov <71787586+Mtchuikov@users.noreply.github.com>
2022-01-24 13:12:54 +0300

    Update decuserbalance.go

--- internal/db/mongodb/decuserbalance.go
+++ internal/db/mongodb/decuserbalance.go
@@ -5,23 +5,19 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"web3pay/internal/db"
 )
-
 func (c *Client) DecUserBalance(ctx context.Context, userId string, balanceChange float64) (*db.UserBalance, error) {
 	coll := c.m.Database("payment").Collection("userInfo")
 	result := coll.FindOneAndUpdate(
 		ctx,
-		bson.D{{"userId", userId}},
-		bson.D{{
-			"$dec", bson.D{{"balance", balanceChange}},
-		}},
+		bson.M{"userId": userId},
+		bson.M{
+			"$dec": bson.M{"balance": balanceChange},
+		},
 	)
 	if result.Err() != nil {
 		return nil, result.Err()
 	}
 	var updatedBalance *db.UserBalance
 	err := result.Decode(&updatedBalance)
-	if err != nil {
-		return nil, err
-	}
-	return updatedBalance, nil
+	return updatedBalance, err
 }
